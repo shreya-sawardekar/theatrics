d36c10f66ab09f4e82cb787c95f77d3e316f97b4
shreya-sawardekar <shreya_sawardekar@proton.me>
2026-04-07 22:12:23 +0530

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Watchlist } from './watchlist/watchlist';
+import { NotFound } from './not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'watch-list',
+    component: Watchlist,
+  },
+  {
+    path: '**',
+    component: NotFound,
+  },
+];
---
+++ src/app/watchlist/watchlist.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-watchlist',
+  imports: [],
+  templateUrl: './watchlist.html',
+  styleUrl: './watchlist.css',
+})
+export class Watchlist {}
